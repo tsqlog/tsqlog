8219b576665364711051c71721be5b44adfeb12c
Bartosz Lepa <bartosz.lepa@gmail.com>
2018-09-01 12:26:02 +0200

    Added log level exists function

--- src/db/tlog/programmability/function/scalar/tlog_conf/tlog_conf.log_level_is_exists.sql
+++ src/db/tlog/programmability/function/scalar/tlog_conf/tlog_conf.log_level_is_exists.sql
@@ -1,7 +1,7 @@
 set nocount on
 if objectproperty(object_id(N'[tlog_conf].[log_level_is_exists]'),'IsScalarFunction') IS NULL
 begin
-	exec (N'create function [tlog_conf].[log_level_is_exists] returns int as begin return null end')
+	exec (N'create function [tlog_conf].[log_level_is_exists]() returns int as begin return null end')
 end
 go
 print 'utc_time:' + convert(varchar(23), getutcdate(), 121) + ';time:' + convert(varchar(23),getdate(),121) + ';db:' + db_name() + ';user:' + suser_sname() + ';msg: creating function [tlog_conf].[log_level_is_exists]'
