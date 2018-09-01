1b2a63006cd96c6e1c5ed4f3bc49ac05ab0c39f8
Bartosz Lepa <bartosz.lepa@gmail.com>
2018-09-01 13:39:13 +0200

    Create log level set

--- src/db/tlog/programmability/stored_procedure/tlog_conf/tlog_conf.log_level_set.sql
+++ src/db/tlog/programmability/stored_procedure/tlog_conf/tlog_conf.log_level_set.sql
@@ -8,14 +8,33 @@ print 'utc_time:' + convert(varchar(23), getutcdate(), 121) + ';time:' + convert
 go
 alter procedure [tlog_conf].[log_level_set]
 (
-	@param1 int 
+	@log_level_code char(50)
+,	@int_level int
+,	@is_active bit 
+,	@user_name nvarchar(128) = null
 )
 as
 begin
 	set nocount on
 
-	return 0
+	select	@user_name = isnull(@user_name, suser_sname())
+
+	update	l
+	set		l.int_level = @int_level
+		,	l.is_active = @is_active
+		,	l.modify_user = @user_name
+		,	l.modify_date = getdate()
+	from	tlog_conf.log_level l
+	where	l.log_level_code = @log_level_code
+	and		(l.int_level != @int_level or l.is_active != @is_active)	
+	
+	insert 
+	into	tlog_conf.log_level(log_level_code,int_level,is_active,create_date,create_user,modify_date,modify_user)
+	select	@log_level_code, @int_level, @is_active, getdate(), @user_name, getdate(), @user_name
+	where	not exists ( select 1 from tlog_conf.log_level where log_level_code = @log_level_code )
+	
 end
 go
 print 'utc_time:' + convert(varchar(23), getutcdate(), 121) + ';time:' + convert(varchar(23),getdate(),121) + ';db:' + db_name() + ';user:' + suser_sname() + ';msg: created procedure [tlog_conf].[log_level_set]'
 go
+
